3d6a572b71fb0c90cf55bfdc2d7acf5690edb1f4
Juan David Merchan Torres <juan1014166@gmail.com>
2025-01-03 17:40:25 -0500

    feat(src): Add increase video speed keymap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log('Hello via Bun!');
+api.mapkey('<Ctrl-a>', 'Increase speed', () => {
+  let video = document.querySelector('video');
+
+  if (!video) return;
+
+  video.playbackRate = video.playbackRate + 0.1;
+
+  api.Front.showBanner(`${video.playbackRate.toFixed(1)}x`);
+});
