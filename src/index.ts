ea7635d563a0a08aa7922a1fa0337eee0aad64bf
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-06 19:17:50 -0500

    refactor(src): Change to use untrap moduls

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-api.unmap('<Ctrl-h>');
-api.unmap('s', /track.toggl.com\/timer/);
-api.mapkey('<Backspace>yl', '[Y]ank [L]ink', () => {
+import { getPageTitle, getPageURL } from "./page";
+import { whatsAppUntrap } from "./untrap/whatsapp";
+import { youTubeUnTrap } from "./untrap/youtube";
   const title = document.title;
   const href = window.location.href;
   const markdownLink = `[${title}](${href})`;
@@ -48,38 +48,6 @@ api.mapkey("<Backspace>ps", "[P]referred [S]peed", () => {
   api.Front.showBanner(`${video.playbackRate.toFixed(1)}x`);
 });
 
-// [U]n[T]rap
-
-function toggleElement<T extends HTMLElement>(element: T): void {
-  element.style.display === 'none'
-    ? (element.style.display = 'block')
-    : (element.style.display = 'none');
-}
-
-function toggleElements(selectors: string[]): void {
-  selectors.forEach((selector) => {
-    const element: HTMLElement | null = document.querySelector(selector);
-
-    if (element) toggleElement(element);
-  });
-}
-
-api.mapkey(
-  '<Backspace>ut',
-  '[U]n[T]rap WhatsApp',
-  () => {
-    const SELECTORS: string[] = [
-      '[aria-label="Status"]',
-      '[aria-label="Channels"]',
-      '[aria-label="Communities"]',
-    ];
-
-    toggleElements(SELECTORS);
-
-      if (element) element.remove();
-    });
-  },
-  {
-    domain: /web.whatsapp.com/,
-  },
-);
+// Untrap
+whatsAppUntrap();
+youTubeUnTrap();
